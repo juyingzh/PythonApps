6d30eaa3b33f9bf844c0f7abccdf57d6ae395c6e
Juying Zhang <juyingzh@gmail.com>
2023-05-20 21:12:28 +0800

    Add function: get_quantile_in()

--- Functions/get_quantile_in.py
+++ Functions/get_quantile_in.py
@@ -1,13 +1,13 @@
 '''
-get_quantile(data, t, linear=True, method='mean')
+get_quantile_in(data, t, linear=True, method='mean')
 
-返回目标数t在给定列表data上的分位值。
+返回目标数t在给定列表data上的分位值。与get_quantile功能相同，区别在于目标值t在data序列中
 
 Parameters:
 
     data: 给定的数值列表，长度大于1，其中不含空值（None）
 
-    t: 目标数
+    t: 目标数，是data序列中的值。
 
     linear=True：逻辑值，是否去掉重复值后排序
 
@@ -18,12 +18,15 @@ Returns
     分位值, 实数, 在[0, 1]之间
 '''
 
-def get_quantile(data, t, linear=True, method='mean'):
+def get_quantile_in(data, t, linear=True, method='mean'):
+
+    if t not in data:
+        raise ValueError("t not in data")
 
     data = [i for i in data if i]  # 去掉空值，类似pandas的dropna()
 
     if method not in {'mean', 'lower', 'higher'}:
-        raise ValueError
+        raise ValueError("method error")
 
     if linear:
         sorted_data=sorted(set(data))
@@ -31,30 +34,27 @@ def get_quantile(data, t, linear=True, method='mean'):
         sorted_data=sorted(data)
 
     if len(sorted_data)<2:
-        raise ValueError
+        raise ValueError("data is too short")
 
     equal= False
     low = 0
     high = len(sorted_data)-1
     for i in range(len(sorted_data)):
-        if t > sorted_data[i]:
-            low = i
-            continue
-        elif t == sorted_data[i]:
+        if t == sorted_data[i]:
             if equal == False:
                 equal = True
                 low = i
+                high = i
                 continue
             else:
+                high = i
                 continue
         else:
             if equal == True:
-                high = i-1
-                break
+                 break
             else:
-                high = i
-                break
-    # print("low={}, high={}".format(low, high))
+                continue
+    print("low={}, high={}, length={}".format(low, high, len(sorted_data)))
 
     if method=='mean':
         return 0.5*(low+high)/(len(sorted_data)-1)
@@ -71,8 +71,7 @@ if __name__ == '__main__':
     # data = [2, 2]
     # data = [1, 2]
     # data = [1]
-    t = 7 # -3.5 # 3 # 0 # 8 # 0.5 # 3.5 # 2 # 3 # 7 # 4.5 # 6 # 3.5 # 3
-    p = get_quantile(data, t)
-    # p = get_quantile(data, t, linear=False)
+    t = [-3.5, 2, 3, 3.5, 7, 4.5, 6, 8]
+    p = get_quantile_in(data, t[4], linear=False, method='higher')
     # p = get_quantile(data, t, 'invalid')
-    print("Percentile of {} in {} is {}".format(t, data, p))
+    print("Percentile of {} in {} is {}".format(t[4], data, p))
